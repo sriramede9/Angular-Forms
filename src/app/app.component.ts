58d94f81ce0f2a1fb87327686c811a6e4b849747
sriramede9 <only4programming03@gmail.com>
2020-05-09 16:21:54 +0100

    @ViewChild('f',{static:false},ngForm)

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  @ViewChild("f", { static: false }) loadForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = "Superuser";
+  }
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  onSubmit() {
+    console.log(this.loadForm);
+  }
+}
